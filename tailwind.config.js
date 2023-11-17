a426f624108d415b2bd722520bd52e1e1fd2f8a3
victoriaharp <toriharpkc@gmail.com>
2023-11-17 14:53:28 +0800

    main structure and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,11 +23,8 @@ module.exports = {
       '9xl': ['8rem', '1'],
     },
     fontFamily: {
-      'sans': ['proxima-nova', 'ui-sans-serif'],
-      'sans-tight': ['proxima-nova-condensed', 'ui-sans-serif'],
-      'mono': ['Inconsolata', 'ui-mono', 'monospace'],
-      'soft': ['proxima-soft', 'ui-sans-serif'],
-      'soft-tight': ['proxima-soft-condensed', 'ui-sans-serif'],
+      'sans': ['Roboto', 'sans-serif'],
+      'serif': ['proxima-nova-condensed', 'ui-sans-serif'],
     },
     extend: {
       lineHeight: {
